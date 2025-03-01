f0ce85c4e55999ebd0b6baeaa1d7deff7f004ccd
Sharad Pratap Singh <sharadxsinghh@gmail.com>
2025-03-01 23:19:02 +0530

    solution to Bitwise_Check Opposite Signs.c

---
+++ Bitwise_Check Opposite Signs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int a,b,c;
+    scanf("%d %d", &a,&b);
+    
+    c=  (a>>31) ^ (b>>31) ;
+        if(c==0)     
+        {
+            printf("No");
+        }
+    
+    else
+    {
+        printf("Yes");
+    }
+}
