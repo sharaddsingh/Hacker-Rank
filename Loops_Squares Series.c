dc7e368b26bfc6f88ea41efa080728a398487e84
Sharad Pratap Singh <sharadxsinghh@gmail.com>
2025-03-08 23:14:36 +0530

    solution to Loops_Squares Series.c

---
+++ Loops_Squares Series.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int n;
+    scanf("%d", &n);  
+   for(int i=1; i<=n ; i++)
+   {
+       if(i%3==0){
+           ++n;
+       }
+       else
+       {
+           printf("%d ",i*i); 
+       }
+   }
+        
+    
+  
+}
