bb53a44fa606bc552f64b38ebcb38940dc8c4f07
Sharad Pratap Singh <sharadxsinghh@gmail.com>
2025-03-11 00:07:31 +0530

    solution to Pattern_Double-Sided Pyramid.c

---
+++ Pattern_Double-Sided Pyramid.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+  int n;
+    scanf("%d", &n);
+    
+    if(n>1)
+    {
+            for(int i=1; i<=n ; ++i)
+            {
+                
+                for(int j=1 ; j<=n-i ; ++j)
+                {
+                    printf(" ");
+                }
+                
+                for(int k=1 ; k<=i ; ++k)
+                {
+                   printf("*"); 
+                }              
+                
+                for(int j=1 ; j<=2 ; ++j)
+                {
+                    printf(" ");
+                }
+                
+                for(int k=1 ; k<=i ; ++k)
+                {
+                   printf("*"); 
+                }    
+                 printf("\n");
+            }
+    }
+    
+    else
+        printf("Shape Not Possible");
+}
