002dd6b78397f2e8743da809c84f060e0383fa2d
Sharad Pratap Singh <sharadxsinghh@gmail.com>
2025-03-13 23:15:55 +0530

    solution to Pattern_Hollow Diamond with Diagonals.c

---
+++ Pattern_Hollow Diamond with Diagonals.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+    scanf("%d", &n);
+    
+    if(n>1)
+    {
+           for( int i=1 ; i<=n ; ++i) 
+           {
+               for( int j=1 ; j<=n-i ; ++j)
+               {
+                   printf(" ");
+               }
+
+               for( int k=1 ; k<= 2*i-1 ;++k)
+               {
+                  if(k==1 || k==2*i-1 || i==n ||i==k)
+                      printf("*");
+                   else
+                      printf(" "); 
+               }
+               printf("\n");
+           }
+        
+          for( int i=2 ; i<=n ; ++i) 
+          {
+               for( int j=1 ; j<=i-1 ; ++j)
+               {
+                   printf(" ");
+               }
+              
+               for( int k=1 ; k<= 2*(n-i+1)-1 ;++k)
+               {
+                  if(k==1 || k== 2*(n-i+1)-1 || k== n-i+1  )
+                      printf("*");
+                   else
+                      printf(" ");  
+               }
+              
+            printf("\n");  
+          }
+    }
+    
+    else
+        printf("Shape Not Possible");
+}
